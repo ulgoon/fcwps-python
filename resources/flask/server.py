ff2af20480c5653b78ff71c92f8af906e1e908dc
ulgoon <ulgoon89@gmail.com>
2021-02-17 19:38:04 +0900

    feat: Additional route for users
    serve json

--- resources/flask/server.py
+++ resources/flask/server.py
@@ -3,9 +3,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# serve string
 @app.route('/')
 def index():
     return 'Flask works'
+
+# additional route with json
+@app.route('/users')
+def show_users():
+    return {'users':['John Doe', 'Jane Doe']}
 
 if __name__=='__main__':
     app.run(host='localhost', port=8080, debug=True)
