6ce9a082404af1d6a1019a7464dbf47609212f30
ulgoon <ulgoon89@gmail.com>
2021-02-17 19:41:56 +0900

    feat: rendering index
    serve index.html

--- resources/flask/server.py
+++ resources/flask/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # serve string
 @app.route('/')
 def index():
-    return 'Flask works'
+    return render_template('index.html')
 
 # additional route with json
 @app.route('/users')
